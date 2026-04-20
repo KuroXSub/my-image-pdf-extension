eede3bd8dab8750b2e10ed44361772f8f2a2684d
KuroXSub <qurrotaayunliulinuha@gmail.com>
2026-04-20 20:05:51 +0700

    Update index.ts

--- src/background/index.ts
+++ src/background/index.ts
@@ -3,17 +3,16 @@
 chrome.sidePanel.setOptions({ enabled: false });
 
 chrome.sidePanel.setPanelBehavior({ openPanelOnActionClick: false })
-  .catch((error) => console.error(error));
+  .catch(console.error);
 
-// 3. Saat user mengklik ikon ekstensi...
 chrome.action.onClicked.addListener((tab) => {
-  if (tab.id) {
-    chrome.sidePanel.setOptions({
-      tabId: tab.id,
-      path: 'index.html',
-      enabled: true
-    });
-    
-    chrome.sidePanel.open({ tabId: tab.id });
-  }
+  if (!tab.id) return;
+
+  chrome.sidePanel.setOptions({
+    tabId: tab.id,
+    path: 'index.html',
+    enabled: true
+  });
+
+  chrome.sidePanel.open({ tabId: tab.id });
 });
